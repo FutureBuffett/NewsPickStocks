9aabf972f2086e6759cba786acb9ea11c121417e
basisp <144201139+basisp@users.noreply.github.com>
2025-07-05 19:23:30 +0900

    Feat: 기본 RAG 시스템 구현
    * feat: 초기 인프라 설정
    
    * feat: 외부 API(Clova Studio)와 통신하는 Executor 클래스 추가
    
    * feat: poetry 설정 추가
    
    * feat: 데이터 처리 파이프라인 구현
    
    * feat: RAG 답변 구현
    
    * feat: Streamlit 앱 구현
    
    * feat: gitignore 수정

---
+++ src/executors/completion_executor.py
@@ -0,0 +1,29 @@
+import json
+import requests
+
+class CompletionExecutor:
+    def __init__(self, host, api_key, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._request_id = request_id
+
+    def execute(self, completion_request):
+        headers = {
+            'Authorization': self._api_key,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'text/event-stream'
+        }
+
+        final_answer = ""
+        with requests.post(self._host + '/testapp/v3/chat-completions/HCX-005',
+                           headers=headers, json=completion_request, stream=True) as r:
+            for line in r.iter_lines():
+                if line:
+                    decoded_line = line.decode("utf-8")
+                    if decoded_line.startswith("data:"):
+                        event_data = json.loads(decoded_line[len("data:"):])
+                        message_content = event_data.get("message", {}).get("content", "")
+                        if message_content:
+                            final_answer = message_content
+        return final_answer 
---
+++ src/executors/embedding_executor.py
@@ -0,0 +1,31 @@
+import http.client
+import json
+
+class EmbeddingExecutor:
+    def __init__(self, host, api_key, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._request_id = request_id
+
+    def _send_request(self, completion_request):
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': self._api_key,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id
+        }
+
+        conn = http.client.HTTPSConnection(self._host)
+        conn.request('POST', '/testapp/v1/api-tools/embedding/clir-emb-dolphin', json.dumps(completion_request), headers)
+        response = conn.getresponse()
+        result = json.loads(response.read().decode(encoding='utf-8'))
+        conn.close()
+        return result
+
+    def execute(self, completion_request):
+        res = self._send_request(completion_request)
+        if res['status']['code'] == '20000':
+            return res['result']['embedding']
+        else:
+            error_code = res["status"]["code"]
+            error_message = res.get("status", {}).get("message", "Unknown error")
+            raise ValueError(f"오류 발생: {error_code}: {error_message}") 
